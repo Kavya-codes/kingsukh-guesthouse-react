8b2a3c2aee96816f00bfbb2bc113c2cd202402f2
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-26 17:23:43 +0530

    Update Testimonial.tsx

--- src/components/Additional/Testimonial.tsx
+++ src/components/Additional/Testimonial.tsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import './Testimonial.css'; 
 
+interface TestimonialProps {
+  name: string;
+  text: string;
+  imageUrl: string; 
+}
+
+const Testimonial: React.FC<TestimonialProps> = ({ name, text, imageUrl }) => {
+  return (
+    <div className="testimonial">
+      <div className="testimonial-content">
+        <p>{text}</p>
+        <div className="testimonial-author">
+          {imageUrl && <img src={imageUrl} alt={name} className="author-image" />}
+          <h4>{name}</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
