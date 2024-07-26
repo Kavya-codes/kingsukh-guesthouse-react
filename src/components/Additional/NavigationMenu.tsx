68a808cf49dd2faf87d27dcb84d17029d7cf8cde
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-26 17:21:40 +0530

    Update NavigationMenu.tsx

--- src/components/Additional/NavigationMenu.tsx
+++ src/components/Additional/NavigationMenu.tsx
@@ -1,1 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import './NavigationMenu.css'; 
 
+const NavigationMenu: React.FC = () => {
+  return (
+    <nav className="navigation-menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/rooms">Rooms</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationMenu;
