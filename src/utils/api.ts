44cabd4277248092630babbdb3f266f60abc2c5a
Kavya Vaishnav <152010782+Kavya-codes@users.noreply.github.com>
2024-07-27 14:13:39 +0530

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,1 +1,19 @@
+import axios from 'axios'; 
+
+const apiBaseUrl = 'https://api.kingsukhguesthouse.com/v1';
+
+export async function getRooms(): Promise<GetRoomsResponse> {
+  const response = await axios.get(`${apiBaseUrl}/rooms`);
+  return response.data;
+}
+
+export async function getRoomDetails(roomId: string): Promise<GetRoomDetailsResponse> {
+  const response = await axios.get(`${apiBaseUrl}/rooms/${roomId}`);
+  return response.data;
+}
+
+export async function bookRoom(request: BookRoomRequest): Promise<BookRoomResponse> {
+  const response = await axios.post(`${apiBaseUrl}/bookings`, request);
+  return response.data;
+}
 
